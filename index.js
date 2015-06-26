a1d0a93d63718e1e1a61f5ea1d6fd92725fc4510
Mark <mkay581@gmail.com>
2015-06-26 13:46:08 -0400

    remove console log

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 module.exports = function(requireName) {
     var singleton = global['singleton-' + requireName];
-    console.log('test');
     if (!singleton) {
         singleton = global['singleton-' + requireName] = require(requireName);
     }
